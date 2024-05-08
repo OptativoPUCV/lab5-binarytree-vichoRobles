7e0934803f2f65aed5c086352c9f88db78b63a75
Your Name <you@example.com>
2024-05-08 00:34:11 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -39,6 +39,11 @@ TreeNode * createTreeNode(void* key, void * value) {
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
     //new->lower_than = lower_than;
+    TreeMap * new = (TreeMap *)malloc(sizeof(TreeMap));
+    new->root = NULL;
+    new->current = NULL;
+    new->lower_than = lower_than;
+    return new
     return NULL;
 }
 
